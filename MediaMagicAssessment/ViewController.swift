ca533c4ab4abbae7ee0f5ae6b78e041c8bff489e
Subhash Kumar <subhash.kumar@smartdatainc.net>
2020-08-13 11:04:26 +0530

    Initial Commit

---
+++ MediaMagicAssessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediaMagicAssessment
+//
+//  Created by Subhash Kumar on 8/13/20.
+//  Copyright © 2020 Subhash Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
