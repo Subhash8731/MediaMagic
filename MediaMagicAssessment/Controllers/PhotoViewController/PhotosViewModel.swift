375aaa4e3ab8cc07796f331c5804fbe7539b41e3
Subhash Kumar <subhash.kumar@smartdatainc.net>
2020-08-13 14:11:22 +0530

    First commit

---
+++ MediaMagicAssessment/Controllers/PhotoViewController/PhotosViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosViewModel.swift
+//  MediaMagicAssessment
+//
+//  Created by Subhash Kumar on 8/13/20.
+//  Copyright © 2020 Subhash Kumar. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewModel: NSObject {
+
+}
